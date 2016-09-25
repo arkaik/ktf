cf9285b8731db05aaac8fdb5d24fc0cdedc3a79e
arkaik <falc@null.net>
2016-09-26 00:22:17 +0200

    test added

--- test/main.js
+++ test/main.js
@@ -1,11 +1,4 @@
 ktf = require('ktf')
-fs = require('fs');
-
-var file = fs.readFile('Greta Salome - Hear them calling.ktf', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(data);
-});
+file = require('notebook')('arkaik/ktf-sample/1.0.2')
 
 ktf.parse(file)
